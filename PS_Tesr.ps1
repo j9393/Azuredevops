8bc3534bb690662f1a3a0b8ff27b6c7745d5d97f
j9393 <58584325+j9393@users.noreply.github.com>
2021-05-07 10:26:56 +0530

    test powerhsell script

---
+++ PS_Tesr.ps1
@@ -0,0 +1,8 @@
+﻿# This script call the below powershell script
+#--------------"E:\BOX4_Custom_Reports\Parts_Outage\PS_Parts_Outage_Report_Script.ps1"--------------------------
+#--------------"E:\BOX4_Custom_Reports\Stocking_parts\PS_Stocking_Parts_Report_Script.ps1"----------------------
+#--------------"E:\BOX4_Custom_Reports\TechProcessCompliance\PS_TechProcessCompliance_Report_Script.ps1"--------
+
+
+write-host "test- powersgell"
+
